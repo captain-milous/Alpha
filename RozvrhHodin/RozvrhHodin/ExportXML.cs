068f89f5e11556db6351bac86f71cbdc6dd14e13
captain-milous <milda.tesar3@gmail.com>
2023-12-06 13:49:29 +0100

    xml zkouska

---
+++ RozvrhHodin/RozvrhHodin/ExportXML.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RozvrhHodin
+{
+    public class ExportXML
+    {
+        public static void Predmet(List<Predmet> dataList, string fileName)
+        {
+            // Create an instance of XmlSerializer
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Predmet>));
+
+            // Create a StreamWriter to write the XML file
+            using (StreamWriter streamWriter = new StreamWriter(fileName))
+            {
+                // Serialize the list to XML and write it to the file
+                serializer.Serialize(streamWriter, dataList);
+            }
+
+            Console.WriteLine($"Data exported to {fileName}.");
+        }
+    }
+    
+}
--- RozvrhHodin/RozvrhHodin/Program.cs
+++ RozvrhHodin/RozvrhHodin/Program.cs
@@ -14,6 +14,11 @@ namespace RozvrhHodin
         [STAThread]
         static void Main()
         {
+            List<Predmet> predmety = new List<Predmet>();
+            predmety.Add(new Predmet());
+            ExportXML.Predmet(predmety, "C:\\Users\\milda\\source\\repos\\Alpha\\Alpha\\Data\\Predmet\\zkouska.xml");
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
